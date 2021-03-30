82ea9534dabd5c5bdf17b7b4b7b0c0df0d4b2c74
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-30 17:10:53 -0300

    feat: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <React.Fragment>
-      <h1>React</h1>
+      <Header />
+
       <GlobalStyle />
     </React.Fragment>
   );
